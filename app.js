2b18ab4db25303e2dc92caa15a4b8585e651af7d
UzzSarp <70654487+UzzSarp@users.noreply.github.com>
2020-11-11 11:41:42 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li')
+
+  burger.addEventListener('click',()=>{
+    //Toggle Nav
+    nav.classList.toggle('nav-active');
+
+    //Animate links
+    navLinks.forEach((link, index) => {
+      if(link.style.animation) {
+        link.style.animation = ' ';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+      }
+    });
+
+    //Burger animation
+    burger.classList.toggle('toggle');
+  });
+
+}
+
+navSlide();
+
+$(window).scroll(function() {
+if($(this).scrollTop() > 5) {
+    $('.navbar-fixed-top').addClass('opaque');
+} else {
+    $('.navbar-fixed-top').removeClass('opaque');
+}
+});
+
